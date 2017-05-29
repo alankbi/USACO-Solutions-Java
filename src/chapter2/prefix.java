0bd30819b4e90132e5673b69fffa40116d981ad1
AlanBi <alan.bi326@gmail.com>
2017-05-29 16:02:34 -0700

    Chapter 2: The Longest Prefix

---
+++ src/chapter2/prefix.java
@@ -0,0 +1,69 @@
+package chapter2;
+/*
+ID: alan.bi1
+LANG: JAVA
+TASK: prefix
+*/
+import java.io.*;
+import java.util.*;
+
+public class prefix
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader f = new BufferedReader(new FileReader("prefix.in"));
+        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("prefix.out")));
+        StringTokenizer st;
+
+        List<String> prim = new ArrayList<String>();
+
+        while(true)
+        {
+            st = new StringTokenizer(f.readLine());
+            String temp = st.nextToken();
+
+            if(temp.equals("."))
+                break;
+
+            prim.add(temp);
+
+            while(st.hasMoreTokens())
+            {
+                prim.add(st.nextToken());
+            }
+        }
+
+        StringBuilder temp = new StringBuilder();
+        String line;
+
+        while((line = f.readLine()) != null)
+        {
+            temp.append(line);
+        }
+        String str = temp.toString();
+
+        boolean[] max = new boolean[str.length() + 1];
+        max[0] = true;
+
+        int maxLength = 0;
+
+        for(int i = 0; i <= str.length(); i++)
+        {
+            for(int j = 0; j < prim.size(); j++)
+            {
+                int startIndex = i - prim.get(j).length();
+
+                if(startIndex >= 0 && max[startIndex] && str.substring(startIndex, startIndex + prim.get(j).length()).equals(prim.get(j)))
+                {
+                    maxLength = i;
+                    max[i] = true;
+                    break;
+                }
+            }
+        }
+
+        out.println(maxLength);
+
+        out.close();
+    }
+}
